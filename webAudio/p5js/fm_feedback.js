ba3209eb2b0bd4e073743dd11bab9398d2381dd0
swell07 <danyi1019@hotmail.com>
2015-12-04 20:16:54 +0100

    1feedback with depth

--- webAudio/p5js/fm_feedback.js
+++ webAudio/p5js/fm_feedback.js
@@ -10,29 +10,34 @@ function setup(){
 
   carrier1 = new p5.Oscillator('sine');
   carrier1.amp(0);
-  carrier1.freq(1);
+  carrier1.freq(120);
   carrier1.start();
 
   carrier2 = new p5.Oscillator('sine');
+  carrier2.amp(0);
+  carrier2.freq(440);
   carrier2.start();
 
   modulator1 = new p5.Oscillator('sawtooth');
   modulator1.start();
-  modulator1.disconnect();
 
+  modulator1.disconnect();
   carrier1.freq(modulator1);
   carrier1.amp(0.3, 0.01);
 }
 
 function draw(){
-  modFreq1 = floor(mouseY * 0.05) //integer(0,20)
-  feedbackGain1 = map(mouseX, 0, width, 0, 0.3);
-  modAmp = (mouseX, 0, width, 0, 0.6);
+  modFreq1 = floor(mouseY * 0.25) //integer(0,20)
+  feedbackGain1 = map(mouseX, 0, width, -150, 150);
+  modAmp = (mouseX, 0, width, -50, 50);
 
   modulator1.freq(modFreq1);
-  carrier1.amp(feedbackGain1, 0.05);
+  modulator1.amp(feedbackGain1, 0.01);
+  //carrier1.amp(feedbackGain1, 0.05);
+   //carrier1.disconnect();
+   carrier1.amp(modAmp, 0.01)
 
-  carrier2.freq(carrier1);
-  carrier2.amp(modAmp, 0.05);
+   carrier2.freq(carrier1);
+   carrier2.amp(0.4, 0.05);
 
 }
