3ed9a20052740f468ecc5c34dbd13fc7c6a41846
swell07 <danyi1019@hotmail.com>
2015-12-06 21:54:06 +0100

    delay

--- webAudio/p5js/feedback2.js
+++ webAudio/p5js/feedback2.js
@@ -13,7 +13,7 @@ function setup(){
   background(0);
 
   delay = new p5.Delay();
-  //＊改volume control 
+  //＊改volume control
   env = new p5.Env(.01, 0.2, .2, .1);
 
   carrier = new p5.Oscillator('sine');
@@ -25,7 +25,7 @@ function setup(){
   modulator.start();
 
   modulator.disconnect();
-  carrier.freq(modulator1);
+  carrier.freq(modulator);
   delaytime = .12;
   feedbackGain = .4;
 //last parameter-low pass frequency
@@ -37,22 +37,22 @@ function draw(){
 }
 
 function mouseDragged(){
-  modFreq1 = floor(mouseY * 0.1) //integer(0,20)
+  modFreq = floor(mouseY * 0.1) //integer(0,20)
   feedbackGain1 = map(mouseX, 0, width, 0, .8);
   modAmp = (mouseX, 0, width, -50, 10);
-  
+
   var modDepth = map(mouseX, 0, width, -50, -25);
   var filterFreq = map (mouseX, 0, width, 10, 300);
   var filterRes = map(mouseY, 0, height, 15, 5);
 
-  modulator1.freq(modFreq1);
-  modulator1.amp(modDepth, 0.01);
-  
-  carrier1.amp(feedbackGain1, 0.01)
+  modulator.freq(modFreq);
+  modulator.amp(modDepth, 0.01);
+
+  carrier.amp(feedbackGain, 0.01)
 
 }
 
 function mouseReleased(){
-env.play(carrier1);
-carrier1.amp(0, 1.5);
+env.play(carrier);
+carrier.amp(0, 1.5);
 }
