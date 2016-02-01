b82cdd61f3b69b6714bfd6460ff7375fc1189b8f
swell07 <danyi1019@hotmail.com>
2016-02-01 14:19:54 +0100

    Create A1_mic.ino

---
+++ Firmata/A1_mic.ino
@@ -0,0 +1,42 @@
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+ 
+void setup() 
+{
+   Serial.begin(9600);
+}
+ 
+ 
+void loop() 
+{
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+   int temp = volts * 1000;
+   int rate = map(temp, 3, 280, 0, 255);
+
+   Serial.write(rate);
+ 
+   //Serial.println(rate);
+}
